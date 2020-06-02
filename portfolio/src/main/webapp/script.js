c69aaa0b3ae8c96250c55db394430a90684f7491
TBarrett553 <barrett.teanna@gmail.com>
2020-06-02 21:44:07 +0000

    Formatted script based on JS Style Guide

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,19 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a randomized donation link to the page.
  */
 function addDonation() {
-  const donations =
-      ['<a href="https://www.floridajc.org/">Florida Justice Center</a>', 
-      '<a href="https://actionnetwork.org/fundraising/louisville-community-bail-fund/">Louisville Community Bail Fund</a>', 
-      '<a href="https://austinjustice.org/">Austin Justice Coalition</a>', '<a href="https://blacklivesmatters.carrd.co/">Black Lives Matter</a>', 
-      '<a href="https://google.benevity.org/campaigns/1643">Google Give</a>', '<a href=""https://www.blackvisionsmn.org/">Black Visions Collective</a>'];
+  const donations =[
+        '<a href="https://www.floridajc.org/">Florida Justice Center</a>', 
+        '<a href="https://actionnetwork.org/fundraising/louisville-community-bail-fund/">Louisville Community Bail Fund</a>', 
+        '<a href="https://austinjustice.org/">Austin Justice Coalition</a>',
+        '<a href="https://blacklivesmatters.carrd.co/">Black Lives Matter</a>', 
+        '<a href="https://google.benevity.org/campaigns/1643">Google Give</a>', 
+        '<a href=""https://www.blackvisionsmn.org/">Black Visions Collective</a>'
+    ];
 
-  // Pick a random greeting.
+  // Pick a random donation link.
   const donation = donations[Math.floor(Math.random() * donations.length)];
 
   // Add it to the page.
