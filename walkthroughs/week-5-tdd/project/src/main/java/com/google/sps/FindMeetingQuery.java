620c71dfb844ab32e214ea80173a2d4b9093bbe7
TBarrett553 <37396488+TBarrett553@users.noreply.github.com>
2020-06-17 14:07:07 -0500

    Removed White Space

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -19,7 +19,5 @@ import java.util.Collection;
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     throw new UnsupportedOperationException("TODO: Implement this method.");
-    
-    
   }
 }
